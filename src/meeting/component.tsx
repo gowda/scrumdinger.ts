63e2285d19d968fe1bde24e9e3db296f5677f9f8
Basavanagowda Kanur <basavanagowda@gmail.com>
2022-06-21 03:29:54 +0530

    Use meeting length in timer

--- src/meeting/component.tsx
+++ src/meeting/component.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import { DailyScrum } from '../daily-scrum';
 import Progress from './progress';
@@ -7,39 +7,10 @@ import SpeakerProgress from './speaker-progress';
 
 type Props = DailyScrum;
 
-export default ({ attendees }: Props) => {
-  const [intervalID, setIntervalID] = useState<NodeJS.Timeout>();
-  const [elapsed, setElapsed] = useState(0);
-  const [remaining, setRemaining] = useState(900);
-
-  useEffect(() => {
-    if (!intervalID) {
-      const id = setInterval(() => {
-        setElapsed((previous) => previous + 1);
-        setRemaining((previous) => previous - 1);
-      }, 1000);
-      setIntervalID(id);
-    }
-
-    return () => {
-      if (intervalID) {
-        clearInterval(intervalID);
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    if (remaining === 0 && intervalID) {
-      clearInterval(intervalID);
-      setIntervalID(undefined);
-    }
-  }, [remaining]);
-
-  return (
-    <div className='d-flex flex-column justify-content-between py-4'>
-      <TimeProgress elapsed={elapsed} remaining={remaining} />
-      <Progress />
-      <SpeakerProgress attendees={attendees} />
-    </div>
-  );
-};
+export default ({ meetingInfo: { lengthInMinutes }, attendees }: Props) => (
+  <div className='d-flex flex-column justify-content-between py-4'>
+    <TimeProgress lengthInMinutes={lengthInMinutes} />
+    <Progress />
+    <SpeakerProgress attendees={attendees} />
+  </div>
+);
--- src/meeting/time-progress.tsx
+++ src/meeting/time-progress.tsx
@@ -1,28 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col, ProgressBar } from 'react-bootstrap';
 
 import TimeElapsed from './time-elapsed';
 import TimeRemaining from './time-remaining';
 
 interface Props {
-  elapsed: number;
-  remaining: number;
+  lengthInMinutes: number;
 }
 
-export default ({ elapsed, remaining }: Props) => (
-  <>
-    <Row>
-      <Col>
-        <ProgressBar now={elapsed} max={elapsed + remaining} />
-      </Col>
-    </Row>
-    <Row className='justify-content-between'>
-      <Col xs='auto'>
-        <TimeElapsed value={elapsed} />
-      </Col>
-      <Col xs='auto'>
-        <TimeRemaining value={remaining} />
-      </Col>
-    </Row>
-  </>
-);
+export default ({ lengthInMinutes }: Props) => {
+  const [intervalID, setIntervalID] = useState<NodeJS.Timeout>();
+  const [elapsed, setElapsed] = useState(0);
+  const [remaining, setRemaining] = useState(lengthInMinutes * 60);
+
+  useEffect(() => {
+    if (!intervalID) {
+      const id = setInterval(() => {
+        setElapsed((previous) => previous + 1);
+        setRemaining((previous) => previous - 1);
+      }, 1000);
+      setIntervalID(id);
+    }
+
+    return () => {
+      if (intervalID) {
+        clearInterval(intervalID);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (remaining === 0 && intervalID) {
+      clearInterval(intervalID);
+      setIntervalID(undefined);
+    }
+  }, [remaining]);
+
+  return (
+    <>
+      <Row>
+        <Col>
+          <ProgressBar now={elapsed} max={elapsed + remaining} />
+        </Col>
+      </Row>
+      <Row className='justify-content-between'>
+        <Col xs='auto'>
+          <TimeElapsed value={elapsed} />
+        </Col>
+        <Col xs='auto'>
+          <TimeRemaining value={remaining} />
+        </Col>
+      </Row>
+    </>
+  );
+};
