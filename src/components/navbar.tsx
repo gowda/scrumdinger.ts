47c6e57349e8cc105c6bee98209d20f30522e0e5
Basavanagowda Kanur <basavanagowda@gmail.com>
2022-06-21 02:18:04 +0530

    Add home link to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export default () => (
-  <Row className='justify-content-center align-items-end border-bottom py-2 mb-4'>
-    <Col xs='auto' className='px-0'>
-      <img src='/images/logo.png' alt='Scrumdinger' width={64} />
-    </Col>
-    <Col xs='auto' className='px-0'>
-      <h1>Scrumdinger</h1>
+  <Row className='justify-content-center border-bottom py-2 mb-4'>
+    <Col xs='auto'>
+      <Link to='/' style={{ textDecoration: 'none', color: 'inherit' }}>
+        <Row className='align-items-end'>
+          <Col xs='auto' className='px-0'>
+            <img src='/images/logo.png' alt='Scrumdinger' width={64} />
+          </Col>
+          <Col xs='auto' className='px-0'>
+            <h1>Scrumdinger</h1>
+          </Col>
+        </Row>
+      </Link>
     </Col>
   </Row>
 );
